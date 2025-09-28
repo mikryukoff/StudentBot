25e76390a7abb9fb50b546ab200688534e6dbf3e
Denis Mikryukov <den20011709@gmail.com>
2025-09-28 19:01:09 +0500

    Fix Exception errors

--- backend/app/exceptions.py
+++ backend/app/exceptions.py
@@ -6,23 +6,24 @@ from fastapi.exceptions import RequestValidationError
 
 class NotFoundException(HTTPException):
     def __init__(self, detail: str = "Not Found", **kwargs: Any):
-        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=detail, **kwargs)
+        kwargs["detail"] = detail
+        kwargs["status_code"] = status.HTTP_404_NOT_FOUND
+        super().__init__(**kwargs)
 
 class UserNotFoundException(NotFoundException):
     def __init__(self, user_id: Optional[int] = None, **kwargs: Any):
         detail = f"User with id {user_id} doesn't exist." if user_id else "User not found"
-        super().__init__(detail=detail, **kwargs)
+        kwargs["detail"] = detail
+        super().__init__(**kwargs)
 
 #-----------------------------------------------------------------------------------------#
 
 class AuthException(HTTPException):
     """Базовое исключение для ошибок аутентификации"""
     def __init__(self, detail: str = "Authentication error", **kwargs: Any):
-        super().__init__(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=detail,
-            **kwargs
-        )
+        kwargs["detail"] = detail
+        kwargs["status_code"] = status.HTTP_401_UNAUTHORIZED
+        super().__init__(**kwargs)
 
 class InvalidCredentialsException(AuthException):
     """Неверные учетные данные"""
